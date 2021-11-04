191bf81abe180b0bbbd3b655aeb7dcf3bfcf1ce6
Manuel Herdez <herdez@gmail.com>
2021-11-04 14:00:42 -0600

    Pedir carta y detener - listo

--- app.js
+++ app.js
@@ -7,37 +7,52 @@ const btnDetener        = document.querySelector('#detener');
 const cartasJugador     = document.querySelector('.cartasJugador');
 const cartasComputadora = document.querySelector('.cartasComputadora');
 
+const puntosJugador     = document.querySelectorAll('small')[0];
+const puntosComputadora = document.querySelectorAll('small')[1];
+
 // VARIABLES 
 const palo       = ['H','D','S','C'];
 const especiales = ['A','J','Q','K'];
 let mazo         = [];
 
+let totalPuntosJugador     = 0;
+let totalPuntosComputadora = 0;
+
 // EVENTOS CLICK DE LOS BOTONES
 btnNuevoJuego.addEventListener('click', () => {
+   console.clear();
+   mazo = [];
+   crearMazo();
    cartasJugador.innerHTML     = '';
    cartasComputadora.innerHTML = '';
+   totalPuntosJugador          = 0;
+   totalPuntosComputadora      = 0;
+   pintarPuntuacion(puntosJugador, totalPuntosJugador);
+   pintarPuntuacion(puntosComputadora, totalPuntosComputadora);
+   prenderBotones();
 })
 
 btnPedirCarta.addEventListener('click', () => {
-   pintarCarta(cartasJugador);
+   // const cartaNueva = pintarCarta(cartasJugador);
+   // const valor = valorCarta(cartaNueva);
+   // totalPuntosJugador += valor;
+   totalPuntosJugador += valorCarta(pintarCarta(cartasJugador));
+   pintarPuntuacion(puntosJugador, totalPuntosJugador);
+   verificarPuntuacion( totalPuntosJugador );
 })
 
+
 btnDetener.addEventListener('click', () => {
-   pintarCarta(cartasComputadora);
+   // const cartaNueva = pintarCarta(cartasComputadora);
+   // const valor = valorCarta(cartaNueva);
+   // totalPuntosComputadora += valor;
+   // pintarPuntuacion(puntosComputadora, totalPuntosComputadora);
+   apagarBotones();
+   console.log('Es turno de la computadora')
 })
 
 
 // FUNCIONES
-const pintarCarta = (turno) => {
-   if( mazo.length === 0) {
-      console.log('ya no hay cartas')
-      return
-   }
-   const figura = mazo.shift();
-   const carta = document.createElement('img');
-   carta.src   = `./assets/cartas/${ figura }.png`;
-   turno.append(carta);
-}
 
 const crearMazo = () => {
    for(let n = 2; n <= 10; n++) {
@@ -54,4 +69,62 @@ const crearMazo = () => {
    console.log(mazo)
 }
 
+const pintarCarta = (turno) => {
+   if( mazo.length === 0) {
+      console.log('ya no hay cartas')
+      return
+   }
+   const valor = mazo.shift();
+   const carta = document.createElement('img');
+   carta.src   = `./assets/cartas/${ valor }.png`;
+   turno.append(carta);
+   return(valor)
+}
+
+const valorCarta = (data) => {
+   // los numeros valen su denominacion, la J, Q, K valen 10 y el A vale 11
+   let valor = data.substring(0, data.length - 1);
+   if( isNaN(valor) ) {
+      if(valor === 'A') {
+         valor = 11;
+      } else {
+         valor = 10;
+      }
+   } else {
+      valor *= 1;
+   }
+   return valor
+}
+
+const pintarPuntuacion = (turno, puntos) => {
+   turno.textContent = puntos;
+}
+
+const verificarPuntuacion = (puntos) => {
+   if(puntos > 21) {
+      console.log('Perdiste');
+      console.log('Turno de la computadora')
+      apagarBotones();
+   }
+}
+
+const apagarBotones = () => {
+   btnPedirCarta.classList.remove('btnAzul');
+   btnPedirCarta.classList.add('btnAzulDesactivado');
+   btnDetener.classList.remove('btnAzul');
+   btnDetener.classList.add('btnAzulDesactivado');
+   btnPedirCarta.setAttribute('disabled', true);
+   btnDetener.setAttribute('disabled', true);
+}
+
+const prenderBotones = () => {
+   btnPedirCarta.classList.remove('btnAzulDesactivado');
+   btnPedirCarta.classList.add('btnAzul');
+   btnDetener.classList.remove('btnAzulDesactivado');
+   btnDetener.classList.add('btnAzul');
+   btnPedirCarta.removeAttribute('disabled');
+   btnDetener.removeAttribute('disabled');
+}
+
+// INICIO DE PROCESOS
 crearMazo();
